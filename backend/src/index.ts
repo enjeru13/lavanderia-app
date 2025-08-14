90489255fd540a4fbc9800ddfd4aff061bc88548
enjeru13 <angeleduardocegarrataborda@gmail.com>
2025-08-14 13:55:09 -0400

    correcion de la bara al final del enlace

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,7 +20,7 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://lavanderia-app-frontend.netlify.app/'
+  'https://lavanderia-app-frontend.netlify.app'
 ];
 
 const corsOptions = {
